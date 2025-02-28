002f105802e7350bab34ad16ce58e286b79689fc
AnEmortalKid <janmonterrubio@gmail.com>
2025-02-28 08:34:56 -0600

    add first file

---
+++ Editor/Tools/Settings/ScriptSummariesSettings.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptSummariesSettings", menuName = "Script Summaries/Settings", order = 0)]
+public class ScriptSummariesSettings :ScriptableObject
+{
+    
+}
